6f56403aaa5e54ff690e1713759ec88ba20699a8
Louis <me@louiscb.com>
2018-11-23 16:03:56 +0100

    Created Encrypt/Decrypt classes

---
+++ src/SessionDecrypter.java
@@ -0,0 +1,12 @@
+import javax.crypto.CipherInputStream;
+import java.io.InputStream;
+
+public class SessionDecrypter {
+    SessionDecrypter (String key, String iv) {
+
+    }
+
+    CipherInputStream openCipherInputStream (InputStream input) {
+        return null;
+    }
+}
---
+++ src/SessionEncrypter.java
@@ -0,0 +1,20 @@
+import javax.crypto.CipherOutputStream;
+import java.io.OutputStream;
+
+public class SessionEncrypter {
+    SessionEncrypter (Integer keyLength) {
+
+    }
+
+    String encodeKey() {
+        return null;
+    }
+
+    String encodeIV() {
+        return null;
+    }
+
+    CipherOutputStream openCipherOutputStream(OutputStream output) {
+        return null;
+    }
+ }
